d8b2b551e67f6092b5b1ecf11fb911f4dd1e53ed
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-02 19:13:01 -0300

    Funções iniciais

---
+++ src/br/senai/sp/jandira/pitagoras/Pitagoras.java
@@ -0,0 +1,12 @@
+import br.senai.sp.jandira.pitagoras.model.Usuario;
+
+public class Pitagoras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Usuario usuario = new Usuario();
+
+        usuario.obterEscolha();
+    }
+}
---
+++ src/br/senai/sp/jandira/pitagoras/model/Usuario.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.pitagoras.model;
+
+public class Usuario {
+
+    int numeroEscolhido;
+    double valorCateto1;
+    double valorCateto2;
+    double valorHipotenusa;
+
+    public void obterEscolha(){
+
+    }
+}
