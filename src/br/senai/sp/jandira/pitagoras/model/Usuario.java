42171444c1f58968a930e7da03c36e0f6e4d5798
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-02 19:29:43 -0300

    Função de verificação de escolha funcionando

--- src/br/senai/sp/jandira/pitagoras/model/Usuario.java
+++ src/br/senai/sp/jandira/pitagoras/model/Usuario.java
@@ -9,8 +9,9 @@ public class Usuario {
     double valorCateto2;
     double valorHipotenusa;
 
+    Scanner leitor = new Scanner(System.in);
+
     public void obterEscolha(){
-        Scanner leitor = new Scanner(System.in);
         System.out.println("Olá, a calculadora está iniciando...");
         System.out.println("Veja os itens abaixo:");
         System.out.println("[1] Cateto");
@@ -18,6 +19,24 @@ public class Usuario {
         System.out.print("Qual deles você quer descobrir? (digite 1 ou 2): ");
         numeroEscolhido = leitor.nextInt();
 
-        System.out.println(numeroEscolhido);
+        verificarEscolha();
+    }
+
+    public void verificarEscolha(){
+        if (numeroEscolhido == 1){
+            descobrirCateto();
+        } else if (numeroEscolhido == 2){
+            descobrirHipotenusa();
+        } else {
+            System.out.println("Valor inválido!");
+        }
+    }
+
+    public void descobrirCateto(){
+
+    }
+
+    public void descobrirHipotenusa(){
+
     }
 }
