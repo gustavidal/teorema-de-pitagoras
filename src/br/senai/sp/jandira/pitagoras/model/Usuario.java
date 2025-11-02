0d0008adb05affe5b4c5fceeb77d607e0bc57b7d
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-02 19:21:32 -0300

    Função de obtenção de valor funcionando

--- src/br/senai/sp/jandira/pitagoras/model/Usuario.java
+++ src/br/senai/sp/jandira/pitagoras/model/Usuario.java
@@ -1,5 +1,7 @@
 package br.senai.sp.jandira.pitagoras.model;
 
+import java.util.Scanner;
+
 public class Usuario {
 
     int numeroEscolhido;
@@ -8,6 +10,14 @@ public class Usuario {
     double valorHipotenusa;
 
     public void obterEscolha(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Olá, a calculadora está iniciando...");
+        System.out.println("Veja os itens abaixo:");
+        System.out.println("[1] Cateto");
+        System.out.println("[2] Hipotenusa");
+        System.out.print("Qual deles você quer descobrir? (digite 1 ou 2): ");
+        numeroEscolhido = leitor.nextInt();
 
+        System.out.println(numeroEscolhido);
     }
 }
