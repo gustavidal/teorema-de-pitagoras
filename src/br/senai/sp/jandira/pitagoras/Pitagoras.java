6f9669a1e6efc86b19f0b8a0b5a00c71af3e68f8
Gustavo Vidal de Abreu <gustavovidalgva@gmail.com>
2025-11-02 21:54:48 -0300

    Mudança na mensagem no main

--- src/br/senai/sp/jandira/pitagoras/Pitagoras.java
+++ src/br/senai/sp/jandira/pitagoras/Pitagoras.java
@@ -3,7 +3,7 @@ import br.senai.sp.jandira.pitagoras.model.Usuario;
 public class Pitagoras {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Vamos começar!");
 
         Usuario usuario = new Usuario();
 
